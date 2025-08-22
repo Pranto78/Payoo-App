5c522fa67c348b6f27f2654ba31e706e2eee9204
Pranto78 <fshahriyar44@gmail.com>
2025-08-23 00:13:51 +0600

    first commit

---
+++ scripts.js
@@ -0,0 +1,24 @@
+
+document.getElementById('button').addEventListener('click', function(e){
+
+    e.preventDefault();
+
+    const mobileNumber = 123456789;
+    const pinNumber = 1234;
+
+    const mobileNumberValue = document.getElementById('number-field').value;
+    const mobileNumberConverted = parseInt(mobileNumberValue);
+
+    const pinNumberValue = document.getElementById('pin-field').value;
+    const pinNumberConverted = parseInt(pinNumberValue);
+
+    if(mobileNumberConverted === mobileNumber && pinNumberConverted === pinNumber){
+        window.location.href = "./home.html" 
+        
+    }
+
+    else{
+        alert("Invalid pin and number.")
+    }
+
+} );
